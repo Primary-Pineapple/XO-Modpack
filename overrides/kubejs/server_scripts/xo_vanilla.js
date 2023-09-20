c517e88b9444e3380c26d36fb4d7ff57675b6cbf
Primary Pineapple <36147901+Primary-Pineapple@users.noreply.github.com>
2023-09-20 18:07:43 +1000

    add kubejs

---
+++ overrides/kubejs/server_scripts/xo_vanilla.js
@@ -0,0 +1,14 @@
+
+
+//ServerEvents.recipes(event => { 
+
+  //  event.remove({ output: 'minecraft:crafting_table' })
+    
+
+  //})
+
+
+
+
+
+console.info('Loaded Script - Vanilla Modifications')
