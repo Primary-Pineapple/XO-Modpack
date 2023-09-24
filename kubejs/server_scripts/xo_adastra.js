d5c77e41d656a0d91a6a09554234b9a3d317785a
Primary Pineapple <36147901+Primary-Pineapple@users.noreply.github.com>
2023-09-24 14:46:22 +1000

    nuke ad astra recipes

---
+++ kubejs/server_scripts/xo_adastra.js
@@ -0,0 +1,13 @@
+//MODIFIES AD ASTRA RECIPES
+
+ServerEvents.recipes(event => { 
+  
+    //NUKE ALL AD ASTRA RECIPES - WE KEEP NOTHING *evil laugh*
+    event.remove({ mod: 'ad_astra' })
+ 
+
+        
+    })
+    
+    
+console.info('Loaded Script - Ad Astra Modifications')
