b80430eda2de2da5bdf121cc99dff716baec4f1e
Primary Pineapple <36147901+Primary-Pineapple@users.noreply.github.com>
2023-09-24 14:46:40 +1000

    Create xo_vanilla.js

---
+++ kubejs/server_scripts/xo_vanilla.js
@@ -0,0 +1,47 @@
+
+
+ServerEvents.recipes(event => { 
+  
+event.remove({ output: 'minecraft:crafting_table' })
+event.remove({ output: 'minecraft:end_crystal' })
+event.remove({ output: '#minecraft:beds' })
+
+
+
+
+//LOGS TO 2x PLANKS ONLY
+event.forEachRecipe({ type: 'minecraft:crafting_shapeless', input: '#minecraft:logs', output: '#minecraft:planks' },
+ r => {
+    let ingredients = r.originalRecipeIngredients
+    let output = r.originalRecipeResult
+    event.shapeless(Item.of(output.id, 2), ingredients[0]).id(r.getOrCreateId())
+  })
+
+//PLANKS TO 2x STICKS ONLY
+event.forEachRecipe({ type: 'minecraft:crafting_shaped', input: '#minecraft:planks', output: 'minecraft:stick' },
+ r => {
+    let ingredients = r.originalRecipeIngredients
+    let output = r.originalRecipeResult
+    event.shapeless(Item.of(output.id, 2), ingredients[0]).id(r.getOrCreateId())
+  })
+
+
+//CREATE FLINT FROM GRAVEL RECIPE
+event.shapeless(
+    Item.of('minecraft:flint', 1), // Output
+    ['3x minecraft:gravel'] // Input
+  )
+
+  
+
+
+
+
+    
+})
+
+
+
+
+
+console.info('Loaded Script - Vanilla Modifications')
