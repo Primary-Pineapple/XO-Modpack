50e3ece8a66d5b24a166a79c4760a8dce18bffa4
Primary Pineapple <36147901+Primary-Pineapple@users.noreply.github.com>
2023-09-24 14:46:05 +1000

    Hide REI Items

---
+++ kubejs/client_scripts/xo_REI.js
@@ -0,0 +1,23 @@
+//REIEvents.removeCategories(event => {
+
+    
+  //  event.remove("minecraft:plugins/tag")
+    
+    
+  //  ;})
+
+
+
+  //HIDE REI ITEMS
+
+  REIEvents.hide('item', event => {
+    
+    event.hide([
+        'ad_astra:hammer'
+
+
+    ])
+
+
+
+})
